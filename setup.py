129a5308bf8b94fc34993297e63a85313a6a0635
JB <juliano.binder@gmail.com>
2015-10-08 18:44:21 +0100

    form style

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='nirvaris-blog',
-    version='0.8',
+    version='0.801',
     packages=['blog'],
     include_package_data=True,
     license='MIT License',  # example license
