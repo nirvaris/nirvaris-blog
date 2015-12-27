0fe94181b0ae06c3cd20443a6f5713a84cb199e9
JB <juliano.binder@gmail.com>
2015-12-27 10:23:04 +0000

    django upgrade

--- blog/templatetags/form_script.py
+++ blog/templatetags/form_script.py
@@ -2,11 +2,10 @@ from django import template
 from django.template import Context
 from django.template.loader import render_to_string
 
-
 import re
 
 register = template.Library()
 
-@register.inclusion_tag('form-script.html')
+@register.inclusion_tag('tag-form-script.html')
 def form_script():
     return {}
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from .views import PostView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^profile/', include('n_profile.urls')),
     url(r'^(?P<tags>.*)$', PostView.as_view()),
 
 ]
