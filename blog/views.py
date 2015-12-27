b2dca11acb661de5f088eadf9c8bf7a191beab34
JB <juliano.binder@gmail.com>
2015-12-27 10:29:37 +0000

    django upgrade

--- blog/views.py
+++ blog/views.py
@@ -10,31 +10,31 @@ from .forms import CommentForm
 from .models import Post
 
 class PostView(View):
-    
+
     def get(self, request, tags):
-        
+
         tag_list = tags.split('/')
         #pdb.set_trace()
         if Post.objects.filter(relative_url=tag_list[-1]).exists():
-        
+
             post = Post.objects.get(relative_url=tag_list[-1])
             post.access_count += 1
             post.save()
             form_initial = {'post_id': post.id}
-            
+
             #pdb.set_trace()
-            
+
             if request.user.is_authenticated():
                 form_initial['email'] = request.user.email
                 form_initial['name'] = request.user.get_full_name()
-            
+
             form = CommentForm(initial=form_initial)
             form.anti_spam()
-            
+
             request_context = RequestContext(request,{'post':post,'form':form})
 
             return render_to_response(post.template, request_context)
-        
+
         #pdb.set_trace()
 
         if tag_list[0]!='':
@@ -43,26 +43,26 @@ class PostView(View):
                 posts = posts.filter(tags__name__iexact=tag)
         else:
             posts = Post.objects.all()
-            
+
         request_context = RequestContext(request,{'posts':posts})
-        
+
         return render_to_response('posts-tags.html', request_context)
 
     def post(self, request, tags):
-        
+
         form = CommentForm(request.POST)
-        
+
         form_valid = form.is_valid()
         cleaned_data = form.clean()
-            
+
         post = Post.objects.get(id=cleaned_data['post_id'])
-        
+
         if form_valid:
             form.save()
             form = CommentForm(initial={'post_id': post.id})
-        
+
         form.anti_spam()
-        
+
         request_context = RequestContext(request,{'post':post,'form':form})
 
         return render_to_response(post.template, request_context)
